1dc766908f99e795cbbeca1aa674496a704686a8
Martin Pickering <martin.pickering@maginus.com>
2023-01-07 15:02:02 +0000

    Added Content Model

--- .tina/config.js
+++ .tina/config.js
@@ -20,44 +20,140 @@ export default defineConfig({
   schema: {
     collections: [
       {
+        label: "Blog Posts",
+        name: "posts",
+        path: "content/posts",
+        format: "md",
+        defaultItem: () => {
+          return {
+            title: 'New Post',
+            date: new Date().toISOString()
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            description: "The Title of the Blog Post",
+            isBody: false,
+            isTitle: true,
+            required: true,
+            ui: {
+              validate: (value, data)=>{
+                const lengthOfTitle = value?.length || 0
+                if(lengthOfTitle < 3){
+                  return 'A Title must be at least 3 characters long'
+                }
+                if(lengthOfTitle > 100){
+                  return 'A Title must be at most 100 characters long'
+                }
+              }
+            }
+          },
+          {
+            type: "string",
+            name: "summary",
+            label: "Summary",
+            description: "A brief summary of the Blog Post",
+            required: true,
+            ui: {
+              component: "textarea",
+              validate: (value, data)=>{
+                const lengthOfTitle = value?.length || 0
+                if(lengthOfTitle < 3){
+                  return 'The Summary must be at least 3 characters long'
+                }
+                if(lengthOfTitle > 250){
+                  return 'The Summary must be at most 250 characters long'
+                }
+              }
+            }
+          },
+          {
+            label: "Date",
+            name: "date",
+            type: "datetime",
+            required: true,
+            ui: {
+              dateFormat: 'DD/MM/YYYY hh:mm A ZZ'
+            }
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Document Body",
+            description: "This is the document body written in markdown",
+            isBody: true,
+            required: false
+          },
+          {
+            type: "string",
+            name: "categories",
+            label: "Category",
+            description: "The name of a Category",
+            required: true,
+            ui: {
+              validate: (value, data)=>{
+                const lengthOfTitle = value?.length || 0
+                if(lengthOfTitle < 3){
+                  return 'Needs at least 3 characters'
+                }
+                if(lengthOfTitle > 100){
+                  return 'At most 100 characters'
+                }
+              }
+            }
+          },
+          {
+            label: "Tags",
+            name: "tags",
+            type: "string",
+            list: true
+          }
+        ],
+      },
+      {
         label: "Pages",
         name: "pages",
         path: "content",
         fields: [
           {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
+            type: "string",
+            name: "title",
+            label: "Title",
+            description: "The Title of the Page",
+            isBody: false,
+            isTitle: true,
+            required: true,
+            ui: {
+              validate: (value, data)=>{
+                const lengthOfTitle = value?.length || 0
+                if(lengthOfTitle < 3){
+                  return 'A Title must be at least 3 characters long'
+                }
+                if(lengthOfTitle > 100){
+                  return 'A Title must be at most 100 characters long'
+                }
+              }
+            }
           },
-        ],
-      },
-      {
-        label: "Posts",
-        name: "posts",
-        path: "content/posts",
-        fields: [
+          {
+            label: "Date",
+            name: "date",
+            type: "datetime",
+            required: true,
+            ui: {
+              dateFormat: 'DD/MM/YYYY hh:mm A ZZ'
+            }
+          },
           {
             type: "rich-text",
             name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
+            label: "Document Body",
+            description: "This is the document body written in markdown",
             isBody: true,
-          },
-        ],
-      },
-      {
-        label: "Portfolio",
-        name: "portfolio",
-        path: "content/portfolio",
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
+            required: false
           },
         ],
       },
