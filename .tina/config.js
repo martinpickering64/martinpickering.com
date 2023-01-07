a09e2e6d7028891d3e2acc83cddc38cbcb357454
Martin Pickering <martin.pickering@maginus.com>
2023-01-07 11:42:01 +0000

    Initialise TinaCMS

---
+++ .tina/config.js
@@ -0,0 +1,66 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "Pages",
+        name: "pages",
+        path: "content",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        label: "Posts",
+        name: "posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        label: "Portfolio",
+        name: "portfolio",
+        path: "content/portfolio",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
